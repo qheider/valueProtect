6cffff38899ddc6124c7a0b97f53b1e7cd544c1e
Quazi Heider <qheider@gmail.com>
2026-02-12 18:51:26 -0500

    clean up
    I want to completely remove the following modules from the project:
    
    BillingCompany entity and all related files
    
    UserBill entity and all related files
    
    PassProtect entity and all related files
    
    This includes:
    
    Entity classes
    
    Controllers
    
    DTOs
    
    Services
    
    Repositories
    
    Mappers
    
    Configuration references
    
    Security references
    
    Any related enums, validators, or utility classes

--- src/main/java/info/quazi/valueProtect/repository/BillingCompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package info.quazi.valueProtect.repository;
-
-import info.quazi.valueProtect.entity.BillingCompany;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BillingCompanyRepository extends JpaRepository<BillingCompany, Long> {
-}
